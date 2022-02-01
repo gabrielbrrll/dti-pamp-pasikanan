ef4b5e772a27f128f011dd67807da546bb3d2114
gabrielbrrll <gabrielb.ocampo@gmail.com>
2022-02-01 22:52:41 +0800

    Updated metatags

---
+++ components/Meta.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Meta = () => (
+  <Head>
+    {/* <!-- Primary Meta Tags --> */}
+    <title>DTI Pasikatan</title>
+    <meta name="title" content="DTI Pasikatan" />
+    <meta
+      name="description"
+      content="Browse SMEs in Pampanga in this online directory."
+    />
+    <meta
+      property="og:image"
+      content="https://i.ibb.co/tMYcVnk/pasikatan-metaimage.png"
+    />
+
+    {/* <!-- Open Graph / Facebook --> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.pasikatan.com/" />
+    <meta property="og:title" content="DTI Pasikatan" />
+    <meta
+      property="og:description"
+      content="Browse SMEs in Pampanga in this online directory."
+    />
+    <meta
+      property="og:image"
+      content="https://i.ibb.co/tMYcVnk/pasikatan-metaimage.png"
+    />
+
+    {/* <!-- Twitter --> */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://www.pasikatan.com/" />
+    <meta property="twitter:title" content="DTI Pasikatan" />
+    <meta
+      property="twitter:description"
+      content="Browse SMEs in Pampanga in this online directory."
+    />
+    <meta
+      property="twitter:image"
+      content="https://i.ibb.co/tMYcVnk/pasikatan-metaimage.png"
+    />
+  </Head>
+)
+
+export default Meta
